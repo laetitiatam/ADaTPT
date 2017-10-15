79784f3cfe38fec01e611ebb9528eee36b1f8f26
laetitiatam <laetitia.tam@gmail.com>
2017-10-15 11:20:17 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-$(function () {
-    $('.datepicker').datepicker();
+$(function() {
+  $('.datepicker').datepicker();
 });
 
-$('#ex1').slider({
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
+
+// Instantiate a slider
+var mySlider = $("input.slider").bootstrapSlider();
+
+// Call a method on the slider
+var value = mySlider.bootstrapSlider('getValue');
+
+// For non-getter methods, you can chain together commands
+mySlider
+  .bootstrapSlider('setValue', 5)
+  .bootstrapSlider('setValue', 7);
+
+function showDiv() {
+  document.getElementById('maybeDiv').style.display = "block";
+}
